fcd3876f1b4b04ad1a010129b5f7556aa469b4f6
PHAN DINH Jean <jean.phandinh@gmail.com>
2026-04-22 19:07:22 +0200

    Create ex02_conditions.py

---
+++ basics/ex02_conditions.py
@@ -0,0 +1,12 @@
+# Exercice 2 — Conditions
+
+note = 14
+
+if note >= 16:
+    print("Très bien")
+elif note >= 14:
+    print("Bien")
+elif note >= 10:
+    print("Assez bien")
+else:
+    print("Insuffisant")
