2f719f60a31de7091dfc5b49b9413ffab418d1b2
PHAN DINH Jean <jean.phandinh@gmail.com>
2026-04-22 19:17:07 +0200

    ex09_strings_tuples.py

---
+++ basics/ex09_strings_tuples.py
@@ -0,0 +1,12 @@
+# Exercice 9 — Strings et tuples
+
+message = "Bonjour Python"
+coordonnees = (43.12, 5.93)
+
+print("Message :", message)
+print("Première lettre :", message[0])
+print("Longueur du message :", len(message))
+
+print("Coordonnées :", coordonnees)
+print("Latitude :", coordonnees[0])
+print("Longitude :", coordonnees[1])
