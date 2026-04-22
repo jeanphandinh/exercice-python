817c872d9c85917f5efdaf8c747d53227305d2b6
PHAN DINH Jean <jean.phandinh@gmail.com>
2026-04-22 19:22:21 +0200

    ex04_strings.py

---
+++ intermediate/ex04_strings.py
@@ -0,0 +1,9 @@
+# Exercice 4 — Strings
+
+phrase = "python est un langage puissant"
+
+print("Phrase d'origine :", phrase)
+print("Majuscules :", phrase.upper())
+print("Première lettre en majuscule :", phrase.capitalize())
+print("Nombre de caractères :", len(phrase))
+print("Remplacement :", phrase.replace("puissant", "utile"))
