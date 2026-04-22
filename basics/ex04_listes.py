3ae7831cec88d2356ac40664a1e0d331e70ccbfc
PHAN DINH Jean <jean.phandinh@gmail.com>
2026-04-22 19:09:35 +0200

    ex04_listes.py

---
+++ basics/ex04_listes.py
@@ -0,0 +1,8 @@
+# Exercice 4 — Listes
+
+fruits = ["pomme", "banane", "orange"]
+
+print("Liste complète :", fruits)
+print("Premier fruit :", fruits[0])
+print("Deuxième fruit :", fruits[1])
+print("Nombre de fruits :", len(fruits))
