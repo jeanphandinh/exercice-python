e69fe702b3cc19748c6c3dbe02e8b7d28a120bdc
PHAN DINH Jean <jean.phandinh@gmail.com>
2026-04-22 19:15:46 +0200

    ex07_dictionnaires.py

---
+++ basics/ex07_dictionnaires.py
@@ -0,0 +1,11 @@
+# Exercice 7 — Dictionnaires
+
+personne = {
+    "nom": "Jean",
+    "age": 18,
+    "ville": "Toulon"
+}
+
+print("Nom :", personne["nom"])
+print("Âge :", personne["age"])
+print("Ville :", personne["ville"])
