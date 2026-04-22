ae9f63f336b3b14c9cfcdabadecc84b28822246e
PHAN DINH Jean <jean.phandinh@gmail.com>
2026-04-22 19:20:12 +0200

    ex01_modules.py

---
+++ intermediate/ex01_modules.py
@@ -0,0 +1,11 @@
+# Exercice 1 — Modules
+
+import math
+
+nombre = 25
+
+racine = math.sqrt(nombre)
+puissance = math.pow(2, 3)
+
+print("Racine carrée de", nombre, ":", racine)
+print("2 puissance 3 :", puissance)
